554047ea5286db56e6022f37238e83da099c3277
shaynes24 <stephhayne23@gmail.com>
2025-02-17 14:48:42 -0500

    third commit

--- src/test.java
+++ src/test.java
@@ -1,10 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("this is the project version 2");
+
+        System.out.println("this is the project version 3");
     }
 }
 //done with version two
