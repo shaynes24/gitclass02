87caf49f0783910e72010f9355571a839f6a3738
shaynes24 <stephhayne23@gmail.com>
2025-02-17 14:39:25 -0500

    first commit

---
+++ src/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("this is the project version 1");
+    }
+}
+//done with version one
