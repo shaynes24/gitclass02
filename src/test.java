943ef19cab73acab7e1bba936c3e7ef1f24b0d11
shaynes24 <stephhayne23@gmail.com>
2025-02-17 14:45:23 -0500

    second commit

--- src/test.java
+++ src/test.java
@@ -1,7 +1,10 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("this is the project version 1");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("this is the project version 2");
     }
 }
-//done with version one
+//done with version two
